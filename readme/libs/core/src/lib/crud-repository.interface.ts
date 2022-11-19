828796890e246c74e348744f440831d70511fb91
Elena <eamatytsina@gmail.com>
2022-11-19 18:36:54 +0300

    fix name

--- readme/libs/core/src/lib/crud-repository.interface.ts
+++ readme/libs/core/src/lib/crud-repository.interface.ts
@@ -1,4 +1,4 @@
-export interface CRUD<E, I, R> {
+export interface CRUDRepository <E, I, R> {
   findById(id: I ):Promise<R | null> ;
   create(item: E): Promise<R>;
   update(id: I, item: E): Promise<R>;
