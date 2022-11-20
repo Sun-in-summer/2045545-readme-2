80bc41be1add086589ac6788c4f01b30888f89cc
Elena <eamatytsina@gmail.com>
2022-11-20 16:48:47 +0300

    feat(blog): создаст заготовки для publication и  blog-post

--- readme/apps/blog/src/app/app.module.ts
+++ readme/apps/blog/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogPostModule } from './blog-post/blog-post.module';
+import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [],
+  imports: [BlogPostModule, PublicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
