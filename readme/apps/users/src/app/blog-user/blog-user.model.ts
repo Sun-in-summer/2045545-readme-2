fb40390699c614d2329e8308500680da5e46a7f4
Elena <eamatytsina@gmail.com>
2022-12-08 20:54:53 +0300

    Добавит  BlogUserModel,  BlogUserRepository.

---
+++ readme/apps/users/src/app/blog-user/blog-user.model.ts
@@ -0,0 +1,52 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User, UserRole } from '@readme/shared-types';
+import {Document} from 'mongoose';
+
+
+
+@Schema({
+  collection: 'users',
+})
+export class BlogUserModel extends Document implements User {
+   @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  firstname: string;
+
+  @Prop({
+    required: true,
+  })
+  lastname: string;
+
+  @Prop({
+    required: true,
+  })
+  birthDate: Date;
+
+  @Prop({
+    required: true,
+  })
+  avatar: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+    default: UserRole.User,
+  })
+  role: UserRole
+
+  @Prop({
+    required: true,
+  })
+  passwordHash: string;
+}
+
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
+
