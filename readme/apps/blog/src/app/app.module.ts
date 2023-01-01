706e46f8f7b89d0383841f0cb953cd75dbd279ca
Elena <eamatytsina@gmail.com>
2023-01-01 21:12:53 +0300

    feat(blog): переформированы таблицы blog

--- readme/apps/blog/src/app/app.module.ts
+++ readme/apps/blog/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { BlogPostModule } from './blog-post/blog-post.module';
 import { BlogCommentModule } from './blog-comment/blog-comment.module';
 
 
 @Module({
   imports: [BlogPostModule, BlogCommentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
